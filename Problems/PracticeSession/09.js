071f08236796da24fb4221051ac77ebe6e801e3d
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2025-07-14 23:14:53 +0530

    string reverse

--- Problems/PracticeSession/09.js
+++ Problems/PracticeSession/09.js
@@ -1,1 +1,10 @@
-// Given a string, reverse each word in the sentence.
+function reverseEachWord(sentence) {
+    return sentence
+        .split(' ')
+        .map(word => word.split('').reverse().join(''))
+        .join(' ');
+}
+
+// Example usage:
+const input = "Hello World";
+console.log(reverseEachWord(input)); // "olleH dlroW"
